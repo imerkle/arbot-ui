4c5b3355c2548c0dc48a3b303a0e34def99c94ac
imerkle <imerkle@tutanota.com>
2019-12-16 04:49:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './App.css';
+import {
+  HashRouter,
+  Route,
+} from 'react-router-dom';
+import { API } from './constants';
+import axios from 'axios';
+
+class App extends React.Component {
+  render() {
+    return (
+      <HashRouter>
+        <Route exact path='/' component={Add}></Route>
+      </HashRouter>
+    )
+  }
+}
+class Add extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hashes: "",
+      response: "",
+    }
+  }
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Arweave Data Archiver</h1>
+        <p>Periodically archives endpoint to arweave</p>
+        <br />
+        <textarea
+          style={{
+            height: 300,
+            width: 600,
+            borderWidth: 0,
+            fontSize: 15,
+          }}
+          onChange={(e) => {
+            this.setState({ hashes: e.target.value });
+          }} placeholder="Paste url to add to archiver" />
+        <br />
+        <button style={{
+          color: "blue",
+          padding: 10,
+          width: 600,
+          cursor: "pointer",
+        }}
+          onClick={(e) => {
+            this.add(this.state.hashes);
+          }}
+        >Archive this to Arweave</button>
+        <br />
+        <div style={{
+          margin: "auto",
+          marginTop: 30,
+          background: "#cbcbcb",
+          padding: 20,
+          width: 800,
+        }}>
+
+          {this.state.response ? <PrettyPrintJson data={this.state.response}></PrettyPrintJson> : ""}
+        </div>
+      </div>)
+  }
+  add = async (hashes) => {
+    alert("Sent to server! Wait for txid!");
+    try {
+      let respones = (await axios.post(`${API}/add`, {
+        url: hashes
+      })).data;
+
+      this.setState({ response: respones })
+    } catch (e) {
+      alert("Some technical error check console");
+      console.error(e)
+    }
+  }
+}
+
+class PrettyPrintJson extends React.Component {
+  render() {
+    const { data } = this.props;
+    const url = `https://viewblock.io/arweave/tx/${data}`;
+    return (<div>{`Response arweave txid: `}<a href={url}>{data}</a></div>);
+  }
+}
+
+export default App;
